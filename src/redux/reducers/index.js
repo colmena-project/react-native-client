8bb7741109526ff79443ef2ce259948ea0fe02df
Juan Antonio Baracat <juatobaracat@gmail.com>
2020-04-17 03:26:16 -0300

    correción de referencias

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,4 +1,10 @@
 import {combineReducers} from 'redux';
 import * as Navigation from './navigation';
+import * as Auth from './auth';
+import * as Account from './account';
+import * as Stock from './stock';
+import * as Waste from './waste';
 
-export default combineReducers(Object.assign(Navigation));
+export default combineReducers(
+  Object.assign(Auth, Account, Navigation, Stock, Waste),
+);
--- src/redux/reducers/navigation.js
+++ src/redux/reducers/navigation.js
@@ -1,22 +1,11 @@
-import createReducer from '../helpers/createReducer';
-import * as types from '../actions/types';
 import AppRouteConfigs from '../../navigators/AppRouteConfigs';
 
 const firstAction = AppRouteConfigs.router.getActionForPathAndParams('Login');
 const initialNavState = AppRouteConfigs.router.getStateForAction(firstAction);
-
-const loggedInStatus = createReducer(
-  {},
-  {
-    [types.SET_LOGGED_IN_STATE](state, action) {
-      return action;
-    },
-  },
-);
 
 const nav = (state = initialNavState, action) => {
   const nextState = AppRouteConfigs.router.getStateForAction(action, state);
   return nextState || state;
 };
 
-export {loggedInStatus, nav};
+export {nav};
