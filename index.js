5150d02397690bcccba0acfee20bc6c15a7ddf7c
Juan Antonio Baracat <juatobaracat@gmail.com>
2020-04-16 22:50:33 -0300

    Se modifica index para obtener variables de entorno

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ import {name as appName} from './app.json';
 
 import Firebase from './src/utils/firebase';
 
+import Config from 'react-native-config';
+
+import AsyncStorage from '@react-native-community/async-storage';
+
+import Parse from 'parse/react-native';
+
 Firebase.init();
+
+Parse.setAsyncStorage(AsyncStorage);
+
+Parse.initialize(Config.PARSE_CONNECT);
+
+Parse.serverURL = Config.PARSE_URL;
 
 AppRegistry.registerComponent(appName, () => App);
